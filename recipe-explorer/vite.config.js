c9f03c210148a2e4c5e53c08ec1769bc57ed2438
Meirunas Smitas <45215244+Meirunas@users.noreply.github.com>
2025-07-16 09:58:46 +0100

    vercel deployment test

--- recipe-explorer/vite.config.js
+++ recipe-explorer/vite.config.js
@@ -1,16 +1,15 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  
+  base: "/",
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
-
+});
